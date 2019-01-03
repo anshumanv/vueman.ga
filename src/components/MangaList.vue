a9912949c56fc23ea3ed9cc886b66c7512420e2e
Anshuman Verma <anshu.av97@gmail.com>
2019-01-03 22:30:19 +0530

    Responsive manga page somehow

--- src/components/MangaList.vue
+++ src/components/MangaList.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-layout row>
-    <v-flex :class="{'ma-0': $vuetify.breakpoint.xsOnly, 'ma-4': $vuetify.breakpoint.smAndUp}" md8 sm6 xs12 lg8>
+  <v-layout row md8 sm6 xs12 lg8>
+    <v-flex :class="{'ma-0': $vuetify.breakpoint.xsOnly, 'ml-4': $vuetify.breakpoint.smAndUp}">
       <v-card>
         <v-toolbar>
           <v-toolbar-title>Chapters</v-toolbar-title>
