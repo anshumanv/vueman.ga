f4a0be8c8ae1f0ee45b3115757fc7270a3c2d135
Aditya Prakash <42460356+pAditya198@users.noreply.github.com>
2019-05-13 12:53:39 +0530

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -51,8 +51,8 @@ export default {
     // TODO: Add cornet case checks for loading more mangas
     loadMore: function() {
       const newMangas = this.allMangas.slice(
-        24 * this.page + 1,
-        24 * this.page + 13
+        23 * this.page + 1,
+        23 * this.page + 13
       );
       this.mangas.push(...newMangas);
       this.page++;
