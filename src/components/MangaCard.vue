8e939a75e59137768bacc4995160b43a968ed06d
Jennifer Bland <ratracegrad@gmail.com>
2019-10-30 14:06:51 -0400

    Fixes issue#11. Removed padding around images so that you have the default spacing from the grid layout provided by vuetify. Added a placeholder no image icon for manga that do not have an image

--- src/components/MangaCard.vue
+++ src/components/MangaCard.vue
@@ -20,8 +20,15 @@
           ></v-progress-circular>
         </v-layout>
       </v-img>
-    </router-link>
-    <router-link :to="`/manga/${manga.i}`" :title="manga.t">
+      <v-img v-else class="manga-img" src="noimage.jpg" height="100%"></v-img>
+      <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
+        <v-progress-circular
+          indeterminate
+          color="grey lighten-5"
+        ></v-progress-circular>
+      </v-layout>
+      <!--    </router-link>-->
+      <!--    <router-link :to="`/manga/${manga.i}`" :title="manga.t">-->
       <div class="subheading font-weight-medium white--text text-truncate">
         {{ manga.t }}
       </div>
@@ -38,6 +45,6 @@ export default {
 
 <style scoped>
 .manga-card {
-  margin: 0.5rem;
+  /*margin: 0.5rem;*/
 }
 </style>
